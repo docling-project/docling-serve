72e18e43dc68babfc9793a79bdcf8d052e88bc15
Tiago Santana <54704492+SantanaTiago@users.noreply.github.com>
2025-08-21 15:03:57 +0100

    updated docling_core dep. add concatened documents to split_processing example
    Signed-off-by: Tiago Santana <54704492+SantanaTiago@users.noreply.github.com>

--- examples/split_processing.py
+++ examples/split_processing.py
@@ -12,7 +12,6 @@ from docling_core.types.doc.document import DoclingDocument
 out_dir = Path("examples/splitted_pdf/")
 pages_per_file = 4
 base_url = "http://localhost:5001/v1"
-output_file = Path("examples/merged_pdf.json")
 
 
 class ConvertedSplittedPdf(BaseModel):
@@ -101,70 +100,21 @@ def main():
     for splitted_pdf in splitted_pdfs:
         splitted_pdf.result = get_task_result(splitted_pdf.task_id)
 
-    # TODO: merge using JSON not working atm; currently outputs splited JSON into folder
-    # merged_document = DoclingDocument(name="merged_pdf.pdf")
+    files = []
     for i, splitted_pdf in enumerate(splitted_pdfs):
-        # page_step = i * pages_per_file
         json_content = json.dumps(
             splitted_pdf.result.get("document").get("json_content"), indent=2
         )
         doc = DoclingDocument.model_validate_json(json_content)
-        doc.save_as_json(filename=f"{out_dir}/splited_json_{i}.json")
-    #     for page, page_item in doc.pages.items():
-    #         merged_document.add_page(page_no=page_step+page, size=page_item.size, image=page_item.image)
+        filename = f"{out_dir}/splited_json_{i}.json"
+        doc.save_as_json(filename=filename)
+        files.append(filename)
 
-    #     for group in doc.groups:
-    #         merged_document.add_group(
-    #             label=group.label,
-    #             name=group.name,
-    #             parent=group.parent,
-    #             content_layer=group.content_layer,
-    #         )
+    docs = [DoclingDocument.load_from_json(filename=f) for f in files]
+    concate_doc = DoclingDocument.concatenate(docs=docs)
 
-    #     for text in doc.texts:
-    #         merged_document.add_text(
-    #             label=text.label,
-    #             text=text.text,
-    #             orig=text.orig,
-    #             parent=text.parent,
-    #             content_layer=text.content_layer,
-    #         )
-
-    #     for picture in doc.pictures:
-    #         merged_document.add_picture(
-    #             annotations=picture.annotations,
-    #             image=picture.image,
-    #             caption=picture.captions[0],
-    #             prov=picture.prov,
-    #             parent=picture.parent,
-    #             content_layer=picture.content_layer,
-    #         )
-
-    #     for table in doc.tables:
-    #         merged_document.add_table(
-    #             data=table.data,
-    #             caption=table.captions[0],
-    #             prov=table.prov,
-    #             parent=table.parent,
-    #             label=table.label,
-    #             content_layer=table.content_layer,
-    #         )
-
-    #     for key_value in doc.key_value_items:
-    #         merged_document.add_key_values(
-    #             graph=key_value.graph,
-    #             prov=key_value.prov,
-    #             parent=key_value.parent,
-    #         )
-
-    #     for doc_form in doc.form_items:
-    #         merged_document.add_form(
-    #             graph=doc_form.graph,
-    #             prov=doc_form.prov,
-    #             parent=doc_form.parent,
-    #         )
-
-    # merged_document.save_as_json(output_file)
+    exp_json_file = Path(f"{out_dir}/concatenated.json")
+    concate_doc.save_as_json(exp_json_file)
 
     print("Finished")
 
