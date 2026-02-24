a83179629e4131a5669b974eaca290649793eba6
Paweł Rein <pawel.rein@prezi.com>
2026-02-24 22:24:29 +0100

    fix: configure `failure_ttl` for RQ failed jobs (#519)
    Signed-off-by: Pawel Rein <pawel.rein@prezi.com>

--- docling_serve/orchestrator_factory.py
+++ docling_serve/orchestrator_factory.py
@@ -496,6 +496,7 @@ def get_async_orchestrator() -> BaseOrchestrator:
                     kwargs={"task_data": task_data},
                     job_id=task_id,
                     timeout=14400,
+                    failure_ttl=docling_serve_settings.eng_rq_failure_ttl,
                 )
 
                 await self.init_task_tracking(task)
@@ -511,6 +512,7 @@ def get_async_orchestrator() -> BaseOrchestrator:
             sub_channel=docling_serve_settings.eng_rq_sub_channel,
             scratch_dir=get_scratch(),
             results_ttl=docling_serve_settings.eng_rq_results_ttl,
+            failure_ttl=docling_serve_settings.eng_rq_failure_ttl,
             redis_max_connections=docling_serve_settings.eng_rq_redis_max_connections,
             redis_socket_timeout=docling_serve_settings.eng_rq_redis_socket_timeout,
             redis_socket_connect_timeout=docling_serve_settings.eng_rq_redis_socket_connect_timeout,
