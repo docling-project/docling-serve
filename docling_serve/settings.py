6bd8a27bfb8d4c86959b14f01245a81dcb68ece7
Christoph Auer <cau@zurich.ibm.com>
2026-04-21 10:42:03 +0200

    Expose ray settings for scaledown control
    Signed-off-by: Christoph Auer <cau@zurich.ibm.com>

--- docling_serve/settings.py
+++ docling_serve/settings.py
@@ -213,6 +213,9 @@ class DoclingServeSettings(BaseSettings):
     eng_ray_max_ongoing_requests_per_replica: Optional[int] = None
     eng_ray_upscale_delay_s: float = 30.0
     eng_ray_downscale_delay_s: float = 600.0
+    # None -> use Ray Serve defaults.
+    eng_ray_graceful_shutdown_wait_loop_s: Optional[float] = None
+    eng_ray_graceful_shutdown_timeout_s: Optional[float] = None
     eng_ray_num_cpus_per_actor: float = 1.0
 
     # Fault Tolerance & Retry
