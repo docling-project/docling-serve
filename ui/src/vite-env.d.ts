9177f57c2509c2df6cb4a1e715aea8b983fef2df
Michele Dolfi <dol@zurich.ibm.com>
2026-09-24 11:42:23 +0200

    add missing files
    Signed-off-by: Michele Dolfi <dol@zurich.ibm.com>

---
+++ ui/src/vite-env.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  /** URL of the Docling viewer page that renders `.dclx` archives. */
+  readonly VITE_DCLX_VIEWER_URL?: string;
+}
